fcbeffc030b5b5d10d00c714f9daff34ded9ee66
Raghav-Hebbar <raghavhebbar2468@gmail.com>
2021-03-15 11:38:51 +0530

    detailView

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Task
 
 # Create your views here.
@@ -7,3 +8,8 @@ from .models import Task
 class TaskList(ListView):
     model  = Task
     context_object_name = 'tasks'
+
+class TaskDetail(DetailView):
+    model = Task
+    template_name = 'todo/task.html'
+    context_object_name = 'task'
